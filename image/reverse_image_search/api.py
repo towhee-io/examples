11969c93783b678a269533ecb1f300433654daee
shiyu22 <shiyu.chen@zilliz.com>
2022-04-24 18:38:18 +0800

    Add gradio in botebook
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>
    
    Add gradio in botebook
    
    Signed-off-by: shiyu22 <shiyu.chen@zilliz.com>

--- image/reverse_image_search/api.py
+++ image/reverse_image_search/api.py
@@ -31,27 +31,25 @@ class MilvusInsert:
         return str(mr)
 
 
-with towhee.api('/insert') as api:
+with towhee.api['path']() as api:
     app_insert = (
-        api.as_entity(schema=['path'])
-        .image_decode['path', 'img']()
+        api.image_decode['path', 'img']()
         .image_embedding.timm['img', 'vec'](model_name='resnet50')
         .runas_op['path', 'hash_path'](func=lambda path: abs(hash(path)) % (10 ** 8))  # delete when support String in Milvus2.1
         .milvus_insert[('hash_path', 'vec'), 'mr'](collection=collection_name)
         .select['mr']()
-        .bind(app)
+        .serve('/insert', app)
         )
 
 
-with towhee.api('/search') as api:
+with towhee.api['path']() as api:
     app_search = (
-        api.as_entity(schema=['path'])
-        .image_decode['path', 'img']()
+        api.image_decode['path', 'img']()
         .image_embedding.timm['img', 'vec'](model_name='resnet50')
         .milvus_search['vec', 'result'](collection=collection_name, **search_args)
         .runas_op['result', 'result'](func=lambda res: [x.path for x in res])
         .select['path', 'result']()
-        .bind(app)
+        .serve('/search', app)
         )
 
 
@@ -66,10 +64,10 @@ class MilvusCount:
         return self.collection.num_entities
 
 
-with towhee.api('/count') as api:
+with towhee.api() as api:
     app_count = (
         api.milvus_count(collection=collection_name)
-        .bind(app)
+        .serve('/count', app)
         )
 
 
